eedac5cc43adf023f244e49233367809ded0c05b
UBU11 <uvaisubu11@gmail.com>
2026-03-17 17:01:53 +0530

    publishable option from clerkprovider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={cn("font-sans", figtree.variable)}>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+        <ClerkProvider>
           {children}
         </ClerkProvider>
       </body>
